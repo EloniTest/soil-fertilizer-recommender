b69cfa4476d361300b0f969f12dd5f50d2e3c885
Eloni <dimagolo102@gmail.com>
2026-06-17 19:10:32 +0300

    added function calculate_recommendation

--- calculations.py
+++ calculations.py
@@ -43,3 +43,56 @@ def calculate_fertilizer_dose(element, deficit_level, crop_coefficient):
     }
 
 def calculate_recommendation(crop_name, fact_n, fact_p, fact_k, fact_ph):
+    crop_norms = get_crop_data(crop_name)
+    if not crop_norms:
+        return None
+    (crop_id, 
+     n_min, n_max, 
+     p_min, p_max, 
+     k_min, k_max, 
+     ph_min, ph_max, 
+     coeff_n, coeff_p, coeff_k) = crop_norms
+    
+    report = {
+        "norms": (n_min, n_max, p_min, p_max, k_min, k_max, ph_min, ph_max),
+        "fertilizers": [],
+        "lime": None
+    }
+
+    elements_to_check = [
+        ("N", fact_n, n_min, coeff_n),
+        ("P", fact_p, p_min, coeff_p),
+        ("K", fact_k, k_min, coeff_k)
+    ]
+
+    for element, fact_value, opt_min, crop_coefficient in elements_to_check:
+        # if the actual value in the soil is higher than or equal to the minimum, there is no deficiency
+        if fact_value >= opt_min:
+            continue
+            
+        # launch a chain of calculations: deficit level -> fertilizer weight
+        deficit_level = determine_deficit_level(fact_value, opt_min)
+        fertilizer_data = calculate_fertilizer_dose(element, deficit_level, crop_coefficient)
+        
+        # Adding a position to the final report
+        report["fertilizers"].append(fertilizer_data)
+        
+    if fact_ph < ph_min:
+        # calculate the difference between the norm and the fact
+        gap_ph = ph_min - fact_ph
+        
+        # agronomic rule: every 0.1 pH deficit requires 0.8 tons of lime per hectare
+        lime_dose_tons = round((gap_ph / 0.1) * 0.8, 1)
+        
+        # obtain the ameliorant data from the database
+        lime_name, _, method, notes = get_fertilizer_by_elem("lime")
+        
+        report["lime"] = {
+            "dose": lime_dose_tons,
+            "name": lime_name,
+            "method": method,
+            "notes": notes
+        }
+        
+    return report
+
